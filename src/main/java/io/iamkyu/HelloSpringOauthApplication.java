e87eac6ac962510b03878189f24466eb2b84dd97
kjnam <kyujnam@gmail.com>
2017-09-03 15:17:01 +0900

    init

---
+++ src/main/java/io/iamkyu/HelloSpringOauthApplication.java
@@ -0,0 +1,12 @@
+package io.iamkyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringOauthApplication.class, args);
+    }
+}
