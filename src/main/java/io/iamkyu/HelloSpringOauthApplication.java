736bc8b5e2744e3bf89284a6e0438fe951353c77
kjnam <kyujnam@gmail.com>
2017-09-03 15:55:02 +0900

    토큰저장소를 H2 DB로 변경

--- src/main/java/io/iamkyu/HelloSpringOauthApplication.java
+++ src/main/java/io/iamkyu/HelloSpringOauthApplication.java
@@ -2,8 +2,13 @@ package io.iamkyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @EnableResourceServer
 @EnableAuthorizationServer
@@ -12,5 +17,10 @@ public class HelloSpringOauthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringOauthApplication.class, args);
+    }
+
+    @Bean
+    public TokenStore jdbcTokenStore(DataSource dataSource) {
+        return new JdbcTokenStore(dataSource);
     }
 }
