0326785b9cde38d6d162ddfcb1cae1b530b322e3
kjnam <kyujnam@gmail.com>
2017-12-24 21:10:41 +0900

    RestResource 어노테이션 추가

--- api-server/src/main/java/io/iamkyu/MemberRepository.java
+++ api-server/src/main/java/io/iamkyu/MemberRepository.java
@@ -1,9 +1,11 @@
 package io.iamkyu;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Kj Nam
  */
+@RepositoryRestResource
 public interface MemberRepository extends PagingAndSortingRepository<Member, Long> {
 }
