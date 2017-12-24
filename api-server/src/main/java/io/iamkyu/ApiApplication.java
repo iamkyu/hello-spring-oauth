2520753cef34bfe00c9d3890cfb07470818ac707
kjnam <kyujnam@gmail.com>
2017-12-24 21:11:27 +0900

    auth-server: 토큰스토어를 Jdbc로 변경

--- api-server/src/main/java/io/iamkyu/ApiApplication.java
+++ api-server/src/main/java/io/iamkyu/ApiApplication.java
@@ -1,15 +1,43 @@
 package io.iamkyu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * @author Kj Nam
  */
+@EnableResourceServer
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(MemberRepository memberRepository) {
+        return args -> {
+            memberRepository.save(new Member("name1", "user1", "test user 1"));
+            memberRepository.save(new Member("name2", "user2", "test user 2"));
+            memberRepository.save(new Member("name3", "user3", "test user 3"));
+        };
+    }
+
+    @Bean
+    public ResourceServerConfigurerAdapter resourceServerConfigurerAdapter() {
+        return new ResourceServerConfigurerAdapter() {
+            @Override
+            public void configure(HttpSecurity http) throws Exception {
+                http.headers().frameOptions().disable();
+                http.authorizeRequests()
+                        .antMatchers("/members", "/members/**").access("#oauth2.hasScope('read')")
+                        .anyRequest().authenticated();
+            }
+        };
     }
 }
--- oauth2-server/src/main/java/io/iamkyu/OAuth2Application.java
+++ oauth2-server/src/main/java/io/iamkyu/OAuth2Application.java
@@ -2,18 +2,26 @@ package io.iamkyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * @author Kj Nam
  */
-@EnableResourceServer
 @EnableAuthorizationServer
 @SpringBootApplication
 public class OAuth2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(OAuth2Application.class, args);
+    }
+
+    @Bean
+    public TokenStore jdbcTokenStore(DataSource dataSource) {
+        return new JdbcTokenStore(dataSource);
     }
 }
