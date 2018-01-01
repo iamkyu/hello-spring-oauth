efa1f78ff43d2c36e709fa3224c9be375b45248b
kjnam <kyujnam@gmail.com>
2018-01-01 15:48:27 +0900

    my-auth-server: project settings

---
+++ my-auth-server/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS user (
+  no int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  name VARCHAR(256) UNIQUE NOT NULL ,
+  password VARCHAR(256) NOT NULL
+);
+
+-- INSERT INTO user (name, password) VALUES ("foo", "fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9");
