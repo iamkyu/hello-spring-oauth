2421bc57a5b558b3d74cdeab0e1d3cae5f2b1e76
kjnam <kyujnam@gmail.com>
2018-01-08 21:43:51 +0900

    토큰 저장을 위한 기본 스키마 정의

--- my-auth-server/src/main/resources/schema.sql
+++ my-auth-server/src/main/resources/schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS user (
 );
 
 -- INSERT INTO user (name, password) VALUES ("foo", "fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9");
+
+
+CREATE TABLE IF NOT EXISTS oauth_access_token (
+  token_id VARCHAR(256),
+  token BLOB,
+  member_no VARCHAR(256),
+  refresh_token VARCHAR(256)
+);
